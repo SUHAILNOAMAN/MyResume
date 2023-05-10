b120e9fd6aab39bcf605a8b66d703b78b97eadbc
Badralbadawi <61519595+Badralbadawi@users.noreply.github.com>
2023-05-10 15:29:15 -0700

    Add project files.

---
+++ WebUIDevExtreme/Controllers/SkillsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Core.Entities;
+using Core.InterFace;
+
+namespace WebUIDevExtreme.Controllers
+{
+    public class SkillsController : BaseController<Skills>
+    {
+        public SkillsController(IUnitOfWork<Skills> context) : base(context)
+        {
+        }
+    }
+}
